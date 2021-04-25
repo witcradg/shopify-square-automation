4e611ad1c67ce4edcb976cf2918624976cf8e217
witcradg <witcradg@gmail.com>
2021-04-25 15:13:15 -0700

    Add RestTemplate to stubs

--- src/main/java/io/witcradg/shopifysquareapi/service/CommunicatorServiceImpl.java
+++ src/main/java/io/witcradg/shopifysquareapi/service/CommunicatorServiceImpl.java
@@ -1,27 +1,33 @@
 package io.witcradg.shopifysquareapi.service;
 
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import io.witcradg.shopifysquareapi.entity.RawJsonEntity;
 
 @Service
 public class CommunicatorServiceImpl implements ICommunicatorService {
+	
+	RestTemplate restTemplate = new RestTemplate();
 
 	@Override
 	public void createCustomer(RawJsonEntity rawJsonEntity) {
-		System.out.println("running createCustomer stub");
+		String response = restTemplate.getForObject("https://httpbin.org/ip", String.class);
+		System.out.println("running createCustomer stub: " + response + " \n\n");
 	
 	}
 
 	@Override
 	public void createInvoice(RawJsonEntity rawJsonEntity) {
-		System.out.println("running createInvoice stub");
+		String response = restTemplate.getForObject("https://cat-fact.herokuapp.com/facts", String.class);		
+		System.out.println("running createInvoice stub: " + response + " \n\n");
 		
 	}
 
 	@Override
 	public void sendSms(RawJsonEntity rawJsonEntity) {
-		System.out.println("running sendSms stub");
+		String response = restTemplate.getForObject("https://api.coindesk.com/v1/bpi/currentprice.json", String.class);
+		System.out.println("running sendSms stub: " + response + " \n\n");
 		
 	}
 
